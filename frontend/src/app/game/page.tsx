81bcae5c54bf9cb4c11524b4bc4d5ccdf5ac39e9
LiamS-H <liamsh@gmail.com>
2024-12-16 23:57:51 -0800

    refactor rename GamePage

--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -1,6 +1,6 @@
 import GameForm from "./game-form";
 
-export default function GamePage() {
+export default function GameConfigPage() {
     return (
         <div className="container mx-auto p-4 max-w-md">
             <GameForm />
