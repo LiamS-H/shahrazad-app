32e74ad1fd8797b77d801989810bddffc5cb5794
liams-h <liamsh@gmail.com>
2025-01-07 17:07:22 -0800

    add context menu

---
+++ frontend/src/components/(game)/(context-menus)/deck.tsx
@@ -0,0 +1,52 @@
+import {
+    ContextMenu,
+    ContextMenuCheckboxItem,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuLabel,
+    ContextMenuRadioGroup,
+    ContextMenuRadioItem,
+    ContextMenuSeparator,
+    ContextMenuShortcut,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import { useShahrazadGameContext } from "../../../contexts/game";
+import { ShahrazadActionCase } from "@/types/bindings/action";
+import { getRandomValues } from "crypto";
+import { type ReactNode, useState } from "react";
+import { randomU64 } from "@/lib/random";
+export default function DeckContextMenu({
+    zoneId,
+    children,
+}: {
+    zoneId: string;
+    children: ReactNode;
+}) {
+    const { applyAction } = useShahrazadGameContext();
+    const [open, setOpen] = useState(true);
+
+    return (
+        <ContextMenu modal={open} onOpenChange={setOpen}>
+            <ContextMenuTrigger>{children}</ContextMenuTrigger>
+            <ContextMenuContent>
+                <ContextMenuLabel>Deck</ContextMenuLabel>
+                <ContextMenuSeparator />
+                <ContextMenuItem
+                    onClick={() => {
+                        applyAction({
+                            type: ShahrazadActionCase.Shuffle,
+                            zone: zoneId,
+                            seed: randomU64().toString(),
+                        });
+                    }}
+                >
+                    Shuffle
+                    <ContextMenuShortcut>⌘S</ContextMenuShortcut>
+                </ContextMenuItem>
+            </ContextMenuContent>
+        </ContextMenu>
+    );
+}
--- frontend/src/components/(game)/playmat/Deck/index.tsx
+++ frontend/src/components/(game)/playmat/Deck/index.tsx
@@ -4,6 +4,7 @@ import { useDroppable } from "@dnd-kit/core";
 import StackZone from "../../card-stack";
 import { IDroppableData } from "@/types/interfaces/dnd";
 import { Scrycard } from "react-scrycards";
+import DeckContextMenu from "../../(context-menus)/deck";
 
 export default function Deck(props: { id: ShahrazadZoneId }) {
     const { getZone } = useShahrazadGameContext();
@@ -13,20 +14,22 @@ export default function Deck(props: { id: ShahrazadZoneId }) {
     const { setNodeRef } = useDroppable({ id: props.id, data });
 
     return (
-        <div
-            className="shahrazad-deck"
-            style={{ width: "auto" }}
-            ref={(ref) => setNodeRef(ref)}
-        >
-            <StackZone
-                emptyMessage={() => (
-                    <Scrycard
-                        card={undefined}
-                        symbol_text_renderer={() => null}
-                    />
-                )}
-                cards={zone.cards}
-            />
-        </div>
+        <DeckContextMenu zoneId={props.id}>
+            <div
+                className="shahrazad-deck"
+                style={{ width: "auto" }}
+                ref={(ref) => setNodeRef(ref)}
+            >
+                <StackZone
+                    emptyMessage={() => (
+                        <Scrycard
+                            card={undefined}
+                            symbol_text_renderer={() => null}
+                        />
+                    )}
+                    cards={zone.cards}
+                />
+            </div>
+        </DeckContextMenu>
     );
 }
