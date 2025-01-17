c259e7178efdc5227c67cc9333f0c5c6062d3188
liams-h <liamsh@gmail.com>
2025-01-17 00:42:45 -0800

    fix card visibility when dragging from hand

--- frontend/src/contexts/dnd/overlay.tsx
+++ frontend/src/contexts/dnd/overlay.tsx
@@ -1,27 +1,19 @@
-import { type CSSProperties, useCallback, useRef, useState } from "react";
+import {
+    type CSSProperties,
+    type ReactNode,
+    useCallback,
+    useRef,
+    useState,
+} from "react";
 import { Scrycard, ScryNameCardText, useScrycard } from "react-scrycards";
 import { useShahrazadGameContext } from "../game";
 import { ShahrazadCardId } from "@/types/bindings/card";
 import { clamp } from "@/lib/clamp";
 
-export function DraggableOverlay({ id }: { id: ShahrazadCardId }) {
-    const { getCard } = useShahrazadGameContext();
-    const shah_card = getCard(id);
-    const card = useScrycard(shah_card.card_name);
-
+function OverlayWrapper({ children }: { children: ReactNode }) {
     const animationFrameId = useRef<number | null>(null);
     const mousePosition = useRef<{ x: number; y: number } | null>(null);
     const [rotation, setRotation] = useState({ rotateX: 0, rotateY: 0 });
-
-    const style: CSSProperties = {
-        position: "absolute",
-        width: "fit-content",
-        cursor: "grabbing",
-        boxShadow: "0 0 10px rgba(0,0,0,0.2)",
-        transition: "transform 0.2s ease-out",
-        transform: `perspective(1000px) rotateX(${rotation.rotateX}deg) rotateY(${rotation.rotateY}deg)`,
-        zIndex: 2,
-    };
 
     const handleMouseMove = useCallback(
         (e: React.MouseEvent<HTMLDivElement>) => {
@@ -51,16 +43,39 @@ export function DraggableOverlay({ id }: { id: ShahrazadCardId }) {
         },
         []
     );
-
+    const style: CSSProperties = {
+        position: "absolute",
+        width: "fit-content",
+        cursor: "grabbing",
+        boxShadow: "0 0 10px rgba(0,0,0,0.2)",
+        transition: "transform 0.2s ease-out",
+        transform: `perspective(1000px) rotateX(${rotation.rotateX}deg) rotateY(${rotation.rotateY}deg)`,
+        zIndex: 2,
+    };
     return (
         <div style={style} onMouseMove={handleMouseMove}>
+            {children}
+        </div>
+    );
+}
+
+export function DraggableOverlay({ id }: { id: ShahrazadCardId }) {
+    const { getCard, player_name } = useShahrazadGameContext();
+    const shah_card = getCard(id);
+    const card = useScrycard(shah_card.card_name);
+
+    return (
+        <OverlayWrapper>
             <Scrycard
                 card={card}
                 symbol_text_renderer={ScryNameCardText}
                 flipped={shah_card.state.flipped}
-                faceDown={shah_card.state.face_down}
+                faceDown={
+                    shah_card.state.face_down &&
+                    !shah_card.state.revealed?.includes(player_name)
+                }
                 tapped={shah_card.state.tapped}
             />
-        </div>
+        </OverlayWrapper>
     );
 }
