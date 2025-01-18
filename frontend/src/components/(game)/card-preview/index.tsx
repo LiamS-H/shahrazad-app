af9dc61a8cc4c0d4f1e602c9a7a6b30a10d08385
liams-h <liamsh@gmail.com>
2025-01-18 11:44:26 -0800

    add preview card is flippable

--- frontend/src/components/(game)/card-preview/index.tsx
+++ frontend/src/components/(game)/card-preview/index.tsx
@@ -1,10 +1,18 @@
+import { Button } from "@/components/(ui)/button";
 import { useShahrazadGameContext } from "@/contexts/game";
 import { useSelection } from "@/contexts/selection";
+import { FlipHorizontal } from "lucide-react";
+import { useEffect, useState } from "react";
 import { ScryNameCard } from "react-scrycards";
 
 export default function CardPreview() {
     const { currentPreview, setPreview } = useSelection();
     const { getCard } = useShahrazadGameContext();
+    const [flipped, setFlipped] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        setFlipped(null);
+    }, [currentPreview]);
 
     if (currentPreview === null) return null;
     const shah_card = getCard(currentPreview);
@@ -21,10 +29,27 @@ export default function CardPreview() {
         >
             <ScryNameCard
                 card_name={shah_card.card_name}
-                flipped={shah_card.state.flipped}
+                flipped={flipped === null ? shah_card.state.flipped : flipped}
                 size="xl"
                 animated
             />
+            <div className="relative">
+                <div className="absolute bottom-1 left-1">
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        onClick={() => {
+                            setFlipped(
+                                flipped === null
+                                    ? !shah_card.state.flipped
+                                    : !flipped
+                            );
+                        }}
+                    >
+                        <FlipHorizontal />
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 }
