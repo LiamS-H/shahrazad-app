80a410b1a9bbb2312122e1db14e23d70e31d4241
liams-h <liamsh@gmail.com>
2025-01-29 17:51:01 -0800

    fix make hero adjust to screen size

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,9 @@ import Link from "next/link";
 export default function Home() {
     return (
         <div className="flex flex-col flex-grow justify-center items-center w-full font-[family-name:var(--font-geist-sans)]">
-            <h1 className="text-9xl select-none">Shahrazad</h1>
+            <h1 className="select-none text-7xl sm:text-8xl md:text-9xl">
+                Shahrazad
+            </h1>
             <Link href={"/game/create"}>
                 <Button
                     variant="highlight"
