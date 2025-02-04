e283e46bac539bb8fb203b480972c417c958a751
liams-h <liamsh@gmail.com>
2025-02-04 13:43:24 -0800

    add exile context-menu

---
+++ frontend/src/components/(game)/(context-menus)/exile.tsx
@@ -0,0 +1,93 @@
+import {
+    ContextMenu,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuLabel,
+    // ContextMenuRadioGroup,
+    // ContextMenuRadioItem,
+    ContextMenuSeparator,
+    // ContextMenuShortcut,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuTrigger,
+} from "@/components/(ui)/context-menu";
+import { useShahrazadGameContext } from "@/contexts/game";
+import { ShahrazadActionCase } from "@/types/bindings/action";
+import { type ReactNode, useState } from "react";
+import { usePlayer } from "@/contexts/player";
+import { useSearchContext } from "@/contexts/search";
+import { randomU64 } from "@/lib/utils/random";
+export default function ExileContextMenu({
+    zoneId,
+    children,
+}: {
+    zoneId: string;
+    children: ReactNode;
+}) {
+    const { player } = usePlayer();
+    const { applyAction, getPlaymat, getZone } = useShahrazadGameContext();
+    const { search } = useSearchContext();
+    const playmat = getPlaymat(player);
+    const [contextOpen, setContextOpen] = useState(true);
+    const exile = getZone(zoneId);
+
+    return (
+        <ContextMenu modal={contextOpen} onOpenChange={setContextOpen}>
+            <ContextMenuTrigger>{children}</ContextMenuTrigger>
+            <ContextMenuContent>
+                <ContextMenuLabel>
+                    Exile ({exile.cards.length})
+                </ContextMenuLabel>
+                <ContextMenuSeparator />
+
+                <ContextMenuItem
+                    disabled={exile.cards.length === 0}
+                    onClick={() => {
+                        search(zoneId);
+                    }}
+                >
+                    Search
+                </ContextMenuItem>
+                <ContextMenuSub>
+                    <ContextMenuSubTrigger disabled={exile.cards.length === 0}>
+                        Send to
+                    </ContextMenuSubTrigger>
+                    <ContextMenuSubContent>
+                        <ContextMenuItem
+                            onClick={() => {
+                                applyAction({
+                                    type: ShahrazadActionCase.CardZone,
+                                    cards: exile.cards,
+                                    index: -1,
+                                    destination: playmat.graveyard,
+                                    state: {},
+                                });
+                            }}
+                        >
+                            Graveyard
+                        </ContextMenuItem>
+                        <ContextMenuItem
+                            onClick={() => {
+                                applyAction({
+                                    type: ShahrazadActionCase.CardZone,
+                                    cards: exile.cards,
+                                    index: 0,
+                                    destination: playmat.library,
+                                    state: {},
+                                });
+                                applyAction({
+                                    type: ShahrazadActionCase.Shuffle,
+                                    zone: playmat.library,
+                                    seed: randomU64(),
+                                });
+                            }}
+                        >
+                            Deck Shuffled
+                        </ContextMenuItem>
+                    </ContextMenuSubContent>
+                </ContextMenuSub>
+            </ContextMenuContent>
+        </ContextMenu>
+    );
+}
--- frontend/src/components/(game)/playmat/Exile/index.tsx
+++ frontend/src/components/(game)/playmat/Exile/index.tsx
@@ -4,34 +4,37 @@ import { useState } from "react";
 import VerticalZone from "../../vertical-zone";
 import { ArrowDownToLine } from "lucide-react";
 import { Button } from "@/components/(ui)/button";
+import ExileContextMenu from "../../(context-menus)/exile";
 
 export default function Exile(props: { id: ShahrazadZoneId }) {
     const { getZone } = useShahrazadGameContext();
     const zone = getZone(props.id);
     const [opened, setOpened] = useState(false);
     return (
-        <div className="shahrazad-exile relative w-fit">
-            <div
-                onClick={() => {
-                    setOpened((o) => !o);
-                }}
-            >
-                <VerticalZone
-                    id={props.id}
-                    hidden={zone.cards.length == 0 || !opened}
-                    emptyMessage="exile"
-                />
-            </div>
-            {opened && zone.cards.length > 1 && (
-                <Button
-                    size="icon"
-                    variant="outline"
-                    className="absolute -bottom-2 -left-2 z-10"
-                    onClick={() => setOpened(false)}
+        <ExileContextMenu zoneId={props.id}>
+            <div className="shahrazad-exile relative w-fit">
+                <div
+                    onClick={() => {
+                        setOpened((o) => !o);
+                    }}
                 >
-                    <ArrowDownToLine />
-                </Button>
-            )}
-        </div>
+                    <VerticalZone
+                        id={props.id}
+                        hidden={zone.cards.length == 0 || !opened}
+                        emptyMessage="exile"
+                    />
+                </div>
+                {opened && zone.cards.length > 1 && (
+                    <Button
+                        size="icon"
+                        variant="outline"
+                        className="absolute -bottom-2 -left-2 z-10"
+                        onClick={() => setOpened(false)}
+                    >
+                        <ArrowDownToLine />
+                    </Button>
+                )}
+            </div>
+        </ExileContextMenu>
     );
 }
