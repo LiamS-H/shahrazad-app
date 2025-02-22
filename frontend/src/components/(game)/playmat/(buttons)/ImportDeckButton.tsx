16649544cf275c50dfcb27728ee687e38059020d
liamsh <liamsh@gmail.com>
2025-02-22 15:25:05 -0800

    add better deck importing

--- frontend/src/components/(game)/playmat/(buttons)/ImportDeckButton.tsx
+++ frontend/src/components/(game)/playmat/(buttons)/ImportDeckButton.tsx
@@ -1,11 +1,6 @@
 "use client";
 import { Button } from "@/components/(ui)/button";
 import { Input } from "@/components/(ui)/input";
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/(ui)/popover";
 import { Textarea } from "@/components/(ui)/textarea";
 import { useShahrazadGameContext } from "@/contexts/(game)/game";
 import { usePlayer } from "@/contexts/(game)/player";
@@ -16,9 +11,18 @@ import { importFromUrl } from "@/lib/client/import-deck/importFromUrl";
 import { toast } from "sonner";
 import { Label } from "@/components/(ui)/label";
 import { ShahrazadAction } from "@/types/bindings/action";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/(ui)/dialog";
 
 export function ImportDeckButton() {
-    const { applyAction, getPlaymat } = useShahrazadGameContext();
+    const { applyAction, getPlaymat, active_player } =
+        useShahrazadGameContext();
     const { player } = usePlayer();
     const playmat = getPlaymat(player);
     const [deckstr, setDeckstr] = useState<string>("");
@@ -58,13 +62,23 @@ export function ImportDeckButton() {
 
     const [open, setOpen] = useState(false);
     return (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
                 <Button variant="outline" size="icon">
                     <Import className="h-[1.2rem] w-[1.2rem]" />
                 </Button>
-            </PopoverTrigger>
-            <PopoverContent className="flex flex-col gap-4">
+            </DialogTrigger>
+            <DialogContent className="flex flex-col gap-4">
+                <DialogHeader>
+                    <DialogTitle>
+                        {player === active_player
+                            ? "Import Your Deck"
+                            : `Import ${player}'s Deck`}
+                    </DialogTitle>
+                    <DialogDescription>
+                        Moxfield deck link or deck string supported.
+                    </DialogDescription>
+                </DialogHeader>
                 <form
                     onSubmit={async (e) => {
                         e.preventDefault();
@@ -74,7 +88,7 @@ export function ImportDeckButton() {
                     <Label htmlFor="deck-url">Link</Label>
                     <Input
                         id="deck-url"
-                        placeholder="(only moxfield supported for now)"
+                        placeholder="https://moxfield.com/decks/XXXX"
                         onChange={(e) => setUrl(e.target.value)}
                         value={url}
                     />
@@ -82,7 +96,7 @@ export function ImportDeckButton() {
                 <div>
                     <Label htmlFor="deck-str">Deck String</Label>
                     <Textarea
-                        placeholder="deck string (mtgo works best)"
+                        placeholder={`1 Opt \n1 Consider`}
                         className="min-h-24"
                         value={deckstr}
                         onChange={(e) => setDeckstr(e.target.value)}
@@ -91,7 +105,7 @@ export function ImportDeckButton() {
                 <Button disabled={!url && !deckstr} onClick={importDeck}>
                     Import
                 </Button>
-            </PopoverContent>
-        </Popover>
+            </DialogContent>
+        </Dialog>
     );
 }
