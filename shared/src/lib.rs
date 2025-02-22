1f86348700b9f0472200fb42df6df7bfaf675327
liamsh <liamsh@gmail.com>
2025-02-21 17:43:17 -0800

    fix sepparate into packages

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,21 +1,24 @@
 pub mod export;
 pub mod types;
-// pub mod action {
-//     include!(concat!(env!("OUT_DIR"), "/proto.action.rs"));
-// }
-// pub mod card {
-//     include!(concat!(env!("OUT_DIR"), "/proto.card.rs"));
-// }
-// pub mod playmat {
-//     include!(concat!(env!("OUT_DIR"), "/proto.playmat.rs"));
-// }
-// pub mod ws {
-//     include!(concat!(env!("OUT_DIR"), "/proto.ws.rs"));
-// }
-
-fn test() {
-    // card::ShahrazadCard::default();
-    // playmat::ShahrazadPlaymat::default();
+pub mod proto {
+    pub mod action {
+        include!(concat!(env!("OUT_DIR"), "/action.rs"));
+    }
+    pub mod card {
+        include!(concat!(env!("OUT_DIR"), "/card.rs"));
+    }
+    pub mod game {
+        include!(concat!(env!("OUT_DIR"), "/game.rs"));
+    }
+    pub mod playmat {
+        include!(concat!(env!("OUT_DIR"), "/playmat.rs"));
+    }
+    pub mod ws {
+        include!(concat!(env!("OUT_DIR"), "/ws.rs"));
+    }
+    pub mod zone {
+        include!(concat!(env!("OUT_DIR"), "/zone.rs"));
+    }
 }
 
 #[cfg(test)]
