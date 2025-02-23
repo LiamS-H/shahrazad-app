36fcdc625fdd80d97e7d0c5e44189f072214ca9b
liamsh <liamsh@gmail.com>
2025-02-23 13:26:03 -0800

    fix moxfield imports that contain _- now parsed

--- frontend/src/lib/client/import-deck/importFromUrl.ts
+++ frontend/src/lib/client/import-deck/importFromUrl.ts
@@ -3,17 +3,21 @@
 import { ShahrazadAction } from "@/types/bindings/action";
 import { importMoxfieldUrl } from "./moxfield";
 import { ILocations, toActionList } from "./toActionlist";
+import { toast } from "sonner";
 
 export async function importFromUrl(
     url: string,
     locations: ILocations
 ): Promise<ShahrazadAction[] | null | undefined> {
-    if (url.match(/^https:\/\/moxfield\.com\/decks\/[a-zA-Z0-9]{22}$/)) {
+    if (url.match(/^https:\/\/moxfield\.com\/decks\/[a-zA-Z0-9\_\-]{22}$/)) {
         const cards = await importMoxfieldUrl(url);
         if (!cards) return undefined;
 
         return toActionList(cards, locations);
     }
+    toast("Deck website not supported.");
 
-    return null;
+    return undefined;
 }
+//https://moxfield.com/decks/R6a3e2uNbkijLU8-LNV5Uw
+//https://moxfield.com/decks/WbOxBONUYEehbHg9E83kgA
