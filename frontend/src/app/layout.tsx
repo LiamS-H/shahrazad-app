3cc4df678b2a9124ce5b3eed988f7fa0fdd2ae25
liamsh <liamsh@gmail.com>
2025-03-09 12:08:14 -0700

    add new slogan

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,7 +21,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
     title: "Shahrazad",
-    description: "A new way to play.",
+    description: "A free fast multiplayer playtester.",
 };
 
 export default function RootLayout({
