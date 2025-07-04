60b675d1824dbac732991e1738f3b91defe7f760
liams-h <liamsh@gmail.com>
2025-07-04 15:36:41 -0700

    refactor to simplify sensor

--- frontend/src/contexts/(game)/dnd/sensors.ts
+++ frontend/src/contexts/(game)/dnd/sensors.ts
@@ -9,6 +9,7 @@ import type { MouseEvent } from "react";
 // import type { TouchEvent } from "react";
 import { ShahrazadActionCase } from "@/types/bindings/action";
 import { ISelectionContext } from "../selection";
+import { ShahrazadCard, ShahrazadCardId } from "@/types/bindings/card";
 
 export class MouseSensor extends LibMouseSensor {
     public static ShahContext: IShahrazadGameContext;
@@ -18,58 +19,54 @@ export class MouseSensor extends LibMouseSensor {
             return false;
         }
 
-        // skip for buttons
         let cur: EventTarget | null = event.target;
+
+        // skip for buttons
         while (cur && cur instanceof Element) {
             if (cur.tagName == "BUTTON") return false;
             if (cur instanceof HTMLElement && cur.dataset.nodrag) return false;
             cur = cur.parentElement;
         }
-
-        // tapping
-        if (event.detail >= 2 && event.detail % 2 == 0) {
-            cur = event.target;
-            while (cur && cur instanceof Element) {
-                if (cur instanceof HTMLElement && cur.dataset.shahcard) {
-                    const id = cur.dataset.shahcard;
-                    const shah_card = MouseSensor.ShahContext.getCard(id);
-                    if (shah_card.state.x === undefined) break;
-                    let selectedCards =
-                        MouseSensor.SelectedContext.selectedCards;
-                    if (!selectedCards.includes(id)) {
-                        MouseSensor.SelectedContext.selectCards(null);
-                        selectedCards = [id];
-                    }
-
-                    MouseSensor.ShahContext.applyAction({
-                        type: ShahrazadActionCase.CardState,
-                        cards: selectedCards,
-                        state: { tapped: !shah_card.state.tapped },
-                    });
-                    return false;
-                }
-                cur = cur.parentElement;
-            }
-        }
-
         cur = event.target;
+
+        let shah_card: ShahrazadCard | null = null;
+        let id: ShahrazadCardId | null = null;
+
         while (cur && cur instanceof Element) {
             if (cur instanceof HTMLElement && cur.dataset.shahcard) {
-                const id = cur.dataset.shahcard;
-                const shah_card = MouseSensor.ShahContext.getCard(id);
-                if (
-                    shah_card.state.face_down &&
-                    !shah_card.state.revealed?.includes(
-                        MouseSensor.ShahContext.active_player
-                    )
-                ) {
-                    return true;
-                }
-                MouseSensor.SelectedContext.setPreview(id);
+                id = cur.dataset.shahcard;
+                shah_card = MouseSensor.ShahContext.getCard(id);
             }
             cur = cur.parentElement;
         }
+        if (!shah_card || !id) return false;
 
+        // tapping
+        if (event.detail >= 2 && event.detail % 2 == 0) {
+            if (shah_card.state.x === undefined) return false;
+            let selectedCards = MouseSensor.SelectedContext.selectedCards;
+            if (!selectedCards.includes(id)) {
+                MouseSensor.SelectedContext.selectCards(null);
+                selectedCards = [id];
+            }
+
+            MouseSensor.ShahContext.applyAction({
+                type: ShahrazadActionCase.CardState,
+                cards: selectedCards,
+                state: { tapped: !shah_card.state.tapped },
+            });
+            return false;
+        }
+
+        // preview
+        if (
+            !shah_card.state.face_down ||
+            shah_card.state.revealed?.includes(
+                MouseSensor.ShahContext.active_player
+            )
+        ) {
+            MouseSensor.SelectedContext.setPreview(id);
+        }
         return true;
     }
     public static getHandler() {
