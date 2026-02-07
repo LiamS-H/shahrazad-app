546ec4b94daf00f51495268516a0ae55713d3bb5
liams-h <liamsh@gmail.com>
2026-02-07 09:59:34 +0000

    add build step action

--- build-server.sh
+++
@@ -1,3 +0,0 @@
-docker build . -t shahrazad-server
-docker tag shahrazad-server us-west1-docker.pkg.dev/shahrazad-app/rust-backend-images/shahrazad-server:latest
-docker push us-west1-docker.pkg.dev/shahrazad-app/rust-backend-images/shahrazad-server:latest
