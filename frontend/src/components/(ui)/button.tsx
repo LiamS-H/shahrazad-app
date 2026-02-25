3b10176688c78d2e7092be94fda6f2782f6a9a8a
liams-h <liamsh@gmail.com>
2026-02-25 01:47:58 +0000

    add rounded to secondary

--- frontend/src/components/(ui)/button.tsx
+++ frontend/src/components/(ui)/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
                 outline:
                     "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
                 secondary:
-                    "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+                    "rounded-full bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
                 highlight:
                     "bg-highlight text-highlight-foreground shadow hover:bg-primary hover:text-primary-foreground",
                 ghost: "rounded-full hover:bg-accent hover:text-accent-foreground",
