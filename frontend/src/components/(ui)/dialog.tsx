3b2d85a57af86a19a55b92f56e5e5718cf640a69
liams-h <liamsh@gmail.com>
2026-02-25 00:28:45 +0000

    fix dialog close clipping corner

--- frontend/src/components/(ui)/dialog.tsx
+++ frontend/src/components/(ui)/dialog.tsx
@@ -39,7 +39,7 @@ const DialogContent = React.forwardRef<
         <DialogPrimitive.Content
             ref={ref}
             className={cn(
-                "rounded-tr-xl fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+                "has-[.close-dialog:hover]:rounded-tr-xl transition-none fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
                 className,
             )}
             {...props}
@@ -47,13 +47,9 @@ const DialogContent = React.forwardRef<
             {children}
             <DialogPrimitive.Close
                 asChild
-                className="absolute -right-1 -top-1 opacity-70 ring-offset-background transition-opacity hover:opacity-100"
+                className="close-dialog hover:bg-destructive absolute -right-1 -top-1 opacity-70 ring-offset-background transition-opacity hover:opacity-100"
             >
-                <Button
-                    size="icon"
-                    variant="ghost"
-                    className="hover:bg-destructive"
-                >
+                <Button size="icon" variant="ghost">
                     <X className="h-4 w-4" />
                     <span className="sr-only">Close</span>
                 </Button>
