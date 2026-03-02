44dc985230a897be96e75c34fffd85c6a8213fea
liamsh <liamsh@gmail.com>
2026-03-02 21:17:22 +0000

    cleanup remove comment

--- frontend/src/lib/client/wasm-init.ts
+++ frontend/src/lib/client/wasm-init.ts
@@ -11,12 +11,3 @@ if (typeof window !== "undefined") {
 export function init_wasm(): Promise<void> {
     return wasm_promise;
 }
-
-// export function useWasmIntialized() {
-//     const [initialized, setInitialized] = useState(wasm_isLoaded);
-//     if (wasm_isLoaded) {
-//         return true;
-//     }
-//     wasm_promise.then(() => setInitialized(true));
-//     return initialized;
-// }
