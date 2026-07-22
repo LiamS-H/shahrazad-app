ae5b43e27b0526f4d9450dc385bb0f3d8234b7e1
liams-h <liamsh@gmail.com>
2026-07-22 21:52:31 +0000

    add potential new smaller exile

--- frontend/src/components/(game)/playmat/Exile/index.tsx
+++ frontend/src/components/(game)/playmat/Exile/index.tsx
@@ -111,3 +111,109 @@ export default function Exile(props: { id: ShahrazadZoneId }) {
         );
     }, [zone, opened, props.id, searching, poppedOut, onClose]);
 }
+
+/*
+import { ShahrazadZoneId } from "@/types/bindings/zone";
+import { useZone } from "@/contexts/(game)/game";
+import { useState, useMemo, useCallback } from "react";
+import { BookDashed } from "lucide-react";
+import { Button } from "@/components/(ui)/button";
+import ExileContextMenu from "@/components/(game)/(context-menus)/exile";
+import ZoneWrapper from "../zone-wrapper";
+import { useSearchContext } from "@/contexts/(game)/search";
+import { PoppedOutZone } from "@/components/(game)/out-zone";
+import { IDroppableData } from "@/types/interfaces/dnd";
+import { useDroppable } from "@dnd-kit/core";
+
+export default function Exile(props: { id: ShahrazadZoneId }) {
+    const zone = useZone(props.id);
+    const [poppedOut, setPoppedOut] = useState(false);
+    const { active } = useSearchContext();
+    const searching = active === props.id;
+
+    const onClose = useCallback(() => {
+        setPoppedOut(false);
+    }, [setPoppedOut]);
+
+    return useMemo(() => {
+        if (searching) {
+            return (
+                <div className="w-full">
+                    <Button
+                        onClick={() => setPoppedOut((o) => !o)}
+                        variant="outline"
+                        size="icon"
+                        className="text-muted-foreground"
+                    >
+                        <BookDashed />
+                    </Button>
+                </div>
+            );
+        }
+
+        return (
+            <div className="w-full h-9">
+                <PoppedOutZone
+                    hidden={!poppedOut}
+                    id={props.id}
+                    name="Exile"
+                    onClose={onClose}
+                    pos={{ x: window.innerWidth - 859, y: 80 }}
+                />
+                <ExileContextMenu
+                    cardId={zone.cards.at(-1) ?? -1}
+                    zoneId={props.id}
+                    onPopOut={() => setPoppedOut((o) => !o)}
+                    poppedOut={poppedOut}
+                >
+                    {poppedOut ? (
+                        <Button
+                            onClick={() => setPoppedOut((o) => !o)}
+                            variant="outline"
+                            size="icon"
+                            className="text-muted-foreground"
+                        >
+                            <BookDashed />
+                        </Button>
+                    ) : (
+                        <DroppableExile
+                            id={props.id}
+                            onClick={() => setPoppedOut((o) => !o)}
+                        />
+                    )}
+                </ExileContextMenu>
+            </div>
+        );
+    }, [zone, props.id, searching, poppedOut, onClose]);
+}
+
+function DroppableExile({
+    id,
+    onClick,
+}: {
+    id: ShahrazadZoneId;
+    onClick: () => void;
+}) {
+    const data: IDroppableData = {};
+    const { setNodeRef, isOver } = useDroppable({ id, data });
+
+    return (
+        <ZoneWrapper
+            zoneId={id}
+            className="absolute shahrazad-exile z-20 -m-4 p-4"
+            ref={setNodeRef}
+        >
+            {isOver ? (
+                <div className="scrycard flex flex-col w-[100px]">
+                    <span className="text-muted-foreground">Exile</span>
+                </div>
+            ) : (
+                <Button onClick={onClick} variant="outline" size="icon">
+                    <BookDashed />
+                </Button>
+            )}
+        </ZoneWrapper>
+    );
+}
+
+ */
